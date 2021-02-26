e50023de64784e8c7438da4438b75fbf4c9e5594
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-26 14:18:52 -0800

    add firebase logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import fire from './fire';
 import './App.css';
 
 // Source: https://www.youtube.com/watch?v=cFgoSrOui2M
@@ -10,6 +11,62 @@ const App = () => {
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [hasAccount, setHasAccount] = useState(false);
+
+  // See NPM Firebase: https://www.npmjs.com/package/firebase
+
+  const handleLogin = () => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case 'auth/invalid-email':
+          case 'auth/user-disabled':
+          case 'auth/user-not found':
+            setEmailError(err.message);
+            break;
+          case 'auth/wrong-password':
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleSignup = () => {
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        // eslint-disable-next-line default-case
+        switch (err.code) {
+          case 'auth/email-already-in-use':
+          case 'auth/invalid-email':
+            setEmailError(err.message);
+            break;
+          case 'auth/weak-password':
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser('');
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
 
   return (
     <div className='App'>
