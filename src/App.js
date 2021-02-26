59c863242e07f4eae9c7ef694a89aaef16731467
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-26 14:01:29 -0800

    set up state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // Source: https://www.youtube.com/watch?v=cFgoSrOui2M
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
   return (
     <div className='App'>
       <h1>Reauth</h1>
     </div>
   );
-}
+};
 
 export default App;
